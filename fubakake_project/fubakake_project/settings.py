6a2f5e1ca66e957d40de5c57092fe61546ad7c79
Davi Russiano <dl.russiano@gmail.com>
2026-04-04 15:39:19 -0300

    chore: protect secrets with python-decouple

--- fubakake_project/fubakake_project/settings.py
+++ fubakake_project/fubakake_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-liy*g1)#d4dljb_i87eto2x(uz#1=7!hu8)+_qh17!056mbdrf'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
